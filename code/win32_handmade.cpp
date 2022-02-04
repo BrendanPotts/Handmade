643bc31ac0f08dc44729064919978b3d352cf8c0
BrendanPotts <brendan@brendanpotts.com>
2022-02-04 15:40:02 -0500

    Taking out the windows reberer and added in bones for our own,

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,6 +1,20 @@
 #include <Windows.h>
+#define local_function static 
+#define local_persist static 
+#define global_var static 
 
-LRESULT CALLBACK WindowProc(
+
+global_var bool Running;
+
+local_function void win32ResizeDIBSection(int Width, int Height) {
+
+}
+local_function void win32UpdateWindow(HWND hwnd, int X, int Y, int Width, int Height) {
+
+}
+
+
+LRESULT CALLBACK win32WindowCallback(
     HWND   hwnd,
     UINT   uMsg,
     WPARAM wParam,
@@ -9,20 +23,27 @@ LRESULT CALLBACK WindowProc(
     LRESULT Result = 0;
     switch (uMsg)
     {
-    case WM_SIZE:
-        OutputDebugStringA("WM_SIZE\n");
-        break;
 
-    case WM_DESTROY:
-        OutputDebugStringA("WM_DESTROY\n");
-        break;
+    case WM_SIZE: {
+        RECT ClientRect;
+        GetClientRect(hwnd, &ClientRect);
+        int Height = ClientRect.bottom - ClientRect.top;
+        int Width = ClientRect.right - ClientRect.left;
+        win32ResizeDIBSection(Width, Height);
+
+    }break;
 
     case WM_CLOSE:
-        OutputDebugStringA("WM_CLOSE\n");
+        Running = false;
         break;
+
 
     case WM_ACTIVATEAPP:
         OutputDebugStringA("WM_ACTIVATEAPP\n");
+        break;
+
+    case WM_DESTROY:
+        Running = false;
         break;
 
     case WM_PAINT: {
@@ -32,9 +53,9 @@ LRESULT CALLBACK WindowProc(
         int Y = Paint.rcPaint.top;
         int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
         int Width = Paint.rcPaint.right - Paint.rcPaint.left;
-        PatBlt(DeviceContext, X, Y, Width, Height, WHITENESS);
+        local_persist DWORD Operation = WHITENESS;
+        win32UpdateWindow(hwnd, X, Y, Width, Height);
         EndPaint(hwnd, &Paint);
-
     }break;
 
     default:
@@ -51,7 +72,7 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 
     WNDCLASS WindowClass = {};
     WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
-    WindowClass.lpfnWndProc = WindowProc;
+    WindowClass.lpfnWndProc = win32WindowCallback;
     WindowClass.hInstance = hInstance;
     // WindowClass.hIcon;
     WindowClass.lpszClassName = "HandmadeHeroWindowClass";
@@ -72,7 +93,9 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
             0
         );
         if (WindowHandle) {
-            for (;;) {
+            Running = true;
+            while (Running)
+            {
                 MSG Message;
                 BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
                 if (MessageResult > 0) {
@@ -83,9 +106,6 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
                     break;
                 }
             }
-        }
-        else {
-            // Error Handaling
         }
     }
     else {
